a7aa5590221d6aa2efb3efcec37bfcf0b531e5cb
ade yahya <adeyahyaprasetyo@gmail.com>
2018-03-09 01:22:29 +0700

    setup authService, auth HOC

--- routes.js
+++ routes.js
@@ -1,4 +1,5 @@
 const routes = module.exports = require('next-routes')();
 routes
   .add('index', '/', 'index')
+  .add('login', '/login', 'login')
   .add('about', '/about', 'about');
---
+++ utils/AuthService.js
@@ -0,0 +1,51 @@
+import axios from 'axios';
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+const path = require('path');
+const R = require('ramda');
+
+export default class AuthService {
+  constructor(endpoint) {
+    this.endpoint = R.or(endpoint, 'https://api.probolinggodev.org/');
+    this.login = this.login.bind(this);
+    this.loggedIn = this.loggedIn.bind(this);
+  }
+
+  async login(email, password) {
+    try {
+      const response = await axios.post(path.join(this.endpoint, 'auth'), {email, password});
+      const {data} = response;
+      const {token} = data;
+      this.setToken(token);
+      return Promise.resolve({message: 'success'});
+    } catch (err) {
+      return Promise.reject(err);
+    }
+  }
+
+  loggedIn() {
+    const token = this.getToken();
+    return token === null ? false : true;
+  }
+
+  setToken(token) {
+    if (localStorage) {
+      localStorage.setItem('auth_token', token);
+      return true;
+    }
+    return false;
+  }
+
+  getToken() {
+    if (localStorage) {
+      return localStorage.getItem('auth_token');
+    }
+  }
+
+  logout() {
+    if (localStorage) {
+      localStorage.removeItem('auth_token');
+      return true;
+    }
+    return false;
+  }
+}
