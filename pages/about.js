371ae840d8ba5608a482e4a51912a9c84652e6fe
Ade Yahya Prasetyo <adeyahyaprasetyo@gmail.com>
2018-06-01 00:45:27 +0700

    update about page

--- pages/about.js
+++ pages/about.js
@@ -1,18 +1,24 @@
+// @flow
 import React from 'react';
 import withAuth from '../utils/withAuth';
 import Layout from '../components/Layout';
 import Container from '../components/Container';
+import CoverHeader from '../components/CoverHeader';
+import Article from '../components/Article';
 
-class About extends React.Component {
+class About extends React.Component<{}> {
   render() {
     return (
       <Layout>
+        <CoverHeader
+          title="ProbolinggoDev"
+          subtitle="We are legion of young people from probolinggo that dreaming of something big, acting for something big!"
+          backgroundUrl="/static/assets/images/about-header-probolinggodev.jpg"
+        />
         <Container>
-          <h1>Probolinggo Dev</h1>
-          <p>Merupakan komunitas yang fokus pada bidang pengembangan perangkat lunak di Probolinggo. Komunitas kami mempunyai rasa optimis yang kuat bahwa masyarakat Probolinggo dari berbagai kalangan yang berminat untuk mengembangkan perangkat lunak komputer dapat belajar bersama, mengembangkan kemampuan di bidang teknis pemrograman, kemampuan bersosialisasi mengenalkan dan mempresentasikan program yang telah ia buat, hingga kesiapan menghadapi tes wawancara pekerjaan maupun menerima pekerjaan sebagai pekerja lepas, sehingga siap berkarir menjadi Programmer yang Handal.</p>
-          <h3>Sejarah</h3>
-          <h3>Visi</h3>
-          <h3>Misi</h3>
+          <Article>
+            <p>Merupakan komunitas yang fokus pada bidang pengembangan perangkat lunak di Probolinggo. Komunitas kami mempunyai rasa optimis yang kuat bahwa masyarakat Probolinggo dari berbagai kalangan yang berminat untuk mengembangkan perangkat lunak komputer dapat belajar bersama, mengembangkan kemampuan di bidang teknis pemrograman, kemampuan bersosialisasi mengenalkan dan mempresentasikan program yang telah ia buat, hingga kesiapan menghadapi tes wawancara pekerjaan maupun menerima pekerjaan sebagai pekerja lepas, sehingga siap berkarir menjadi Programmer yang Handal.</p>
+          </Article>
         </Container>
       </Layout>
     );
