ec6b4ea5cb1133c6dadb95c18751c5f0bae4c44f
Ade Yahya <adeyahyaprasetyo@gmail.com>
2019-01-12 00:10:09 +0700

    update projects content

--- server/contents/projects/hayati-bot.js
+++ server/contents/projects/hayati-bot.js
@@ -1,7 +1,7 @@
 module.exports = {
   slug: 'hayati-bot',
   image:
-    'https://opensource.fb.com/templates/dist/images/new_svgs/fasttext.svg',
+    'https://res.cloudinary.com/dt2mntbmf/image/upload/v1547212005/hayati-bot-logo_cgt08n.jpg',
   title: 'Hayati Bot',
   description: 'Telegram Bot for managing community stuff',
   github: 'https://github.com/probolinggo-dev/hayati-bot',
---
+++ server/contents/projects/probolinggodev.org.js
@@ -0,0 +1,8 @@
+module.exports = {
+  slug: 'probolinggodev.org',
+  image:
+    'https://res.cloudinary.com/dt2mntbmf/image/upload/v1547225977/probolinggodev_j218df.png',
+  title: 'ProbolinggoDev.org',
+  description: 'Probolinggo Dev Official Website',
+  github: 'https://github.com/probolinggo-dev/probolinggodev.org',
+};
