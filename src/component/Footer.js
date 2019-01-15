08f26c3496c2520624e21b10271b3a3bae734484
dns <dns@dnss-MacBook-Pro.local>
2019-01-15 15:22:19 +0700

    move ga script to index.js

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-import ReactGA from 'react-ga';
 import Container from './Container';
 
-ReactGA.initialize('UA-132442251-1');
-ReactGA.pageview('/home');
 
 export default () => (
   <Footer>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo-hooks';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import routes from './routes';
 import Footer from './component/Footer';
 import * as serviceWorker from './serviceWorker';
+
+
+ReactGA.initialize('UA-132442251-1');
+ReactGA.pageview('/home');
 
 const isProd = process.env.NODE_ENV === 'production';
 
