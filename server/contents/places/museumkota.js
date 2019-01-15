8dfb72b2f9a2317dcd6998ad6fb6791a3a3ed5b4
AryaTux <rahadian271094@gmail.com>
2019-01-15 08:54:07 +0700

    Museum Kota Probolinggo

---
+++ server/contents/places/museumkota.js
@@ -0,0 +1,20 @@
+module.exports = {
+  slug: 'museumkotaprobolinggo',
+  title: 'Museum Kota Probolinggo',
+  image: 'http://www.eastjava.com/tourism/probolinggo/city-tour/images/museum.jpg',
+  description: 'Museum Probolinggo merupakan destinasi wisata yang menawarkan konsep sejarah dari Probolinggo, Museum ini terletak di Jalan Suroyo No 7 Kota Probolinggo, jadi anda tidak akan sulit untuk menemukan letak dari museum ini, karena akses menuju kesana yang sangat mudah dijangkau, baik oleh wisatawan yang menggunakan kendaraan sendiri atau dengan menaiki kendaraan umum',
+  content: `
+  Museum ini mempunyai arsitektur bangunan yang khas peninggalan Belanda menjadikan Museum ini sangat kental dengan suasana masa lampau. 
+
+  Bangunan Museum ini dulunya merupakan sebuah Gedung Societiet Gebow Harmony yang difungsikan sebagai Ballroom (Kamar Bola), yang artinya merupakan tempat berkumpulnya kaum priyayi Belanda dan kemudian gedung ini difungsikan sebagai Gedung Serba Guna yang biasanya digunakan oleh masyarakat atau instansi di Probolinggo untuk melakukan acara atau event – event tertentu.
+
+  Gedung ini pertama dibangun pada tahun 1814 yang mempunyai gaya bangunan Empire Style. 
+  
+  Museum yang di resmikan tanggal 15 mei 2011 ini memang memiliki banyak informasi sejarah tentang Kota Probolinggo. 
+  
+  Menyusuri Museum ini seakan ikut menjelajah Kota Probolinggo dari zaman dahulu hingga saat ini. 
+  
+  Berwisata sejarah di Museum Probolinggo ini menjadikan kita kaya informasi akan sejarah.
+
+`,
+};
