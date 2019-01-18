f85f354a7e261ab215ec3f6b6e4e8713e29f7c31
Ade Yahya <adeyahyaprasetyo@gmail.com>
2019-01-19 00:03:37 +0700

    scroll restoration mechanism

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo-hooks';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
 import routes from './routes';
 import Footer from './component/Footer';
 import * as serviceWorker from './serviceWorker';
+
+const customHistory = createBrowserHistory();
+let prevLocation;
 
 const isProd = process.env.NODE_ENV === 'production';
 
@@ -13,14 +17,25 @@ const client = new ApolloClient({
   uri: isProd ? `${window.location.origin}/graphql` : 'http://localhost:5000/graphql',
 });
 
+const ScrollRestoration = Page => {
+  return props => {
+    const { location } = props;
+    if (location.pathname !== prevLocation) {
+      window.scrollTo(0, 0);
+    }
+    prevLocation = location.pathname;
+    return <Page {...props} />;
+  };
+};
+
 const App = () => (
-  <Router>
+  <Router history={customHistory}>
     <ApolloProvider client={client}>
       <div>
         {routes.map((item, index) => {
           const { screen, ...rest } = item;
           const Component = require(`./screen/${screen}`).default;
-          return <Route key={index} component={Component} {...rest} />;
+          return <Route key={index} component={ScrollRestoration(Component)} {...rest} />;
         })}
         <Footer />
       </div>
